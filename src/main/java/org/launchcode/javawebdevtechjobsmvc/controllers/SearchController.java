d518db3fa326e47be4dbf515aef36a7ae1037c8e
Anna McBride <mcbrideanna28@gmail.com>
2021-01-22 19:12:32 -0600

    finished assignment

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
+import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.tableChoices;
 
 /**
  * Created by LaunchCode
@@ -25,15 +26,19 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+
     @PostMapping(value = "results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
-       ArrayList<Job> jobs = new ArrayList<>();
-       if(searchTerm == "all" && searchType == null){
+       ArrayList<Job> jobs;
+        if(searchTerm != "jobs") {
+           model.addAttribute("No results");
+        }
+       if(searchTerm == "all" && searchType == ""){
           jobs = JobData.findAll();
        } else {
            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
        }
-       model.addAttribute("defaultChoice", searchTerm);
+
        model.addAttribute("columns", columnChoices);
        model.addAttribute("jobs", jobs);
         return "search";
